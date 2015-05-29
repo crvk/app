a5ca7a1eacc7c0db46f0b3a4f8cc15d7d45673e0
christian k <christian.kaunissaar@dynabyte.se>
2015-05-29 14:27:54 +0200

    changes

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ app = Flask("app")
 api = Api(app)
 Bootstrap(app)
 
+persons = {}
 
 @app.route("/")
 def main():
-    try:
-        persons = Persons(PersonClient.getPersons()).get()
-        return render_template('index.html', name=persons[0]['first_name'], persons=persons)
+    return render_template('index.html', name='hej', persons=persons)
 
-    except: ValueError
-    return render_template('index.html', message='Failed to connect to Person Service')
+@app.route("/fetch")
+def fetch():
+    print('fetch')
+    persons = get_persons()
+    print(persons)
+    return render_template('index.html', name='hej', persons=persons)
 
+
+def get_persons():
+    return Persons(PersonClient.getPersons()).get()
+
+
+app.run(port=8000, debug=True)
